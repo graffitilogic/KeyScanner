4959e685ad84b25b2400ec5d8ac639d23c72bab6
Dev McDevington <bill.dodd@graffitilogic.com>
2022-08-06 16:51:51 -0500

    Samples for BTC32 40Bit

---
+++ Samples/test40.bat
@@ -0,0 +1,10 @@
+
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-0.txt
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-1.txt
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-2.txt
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-3.txt
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-4.txt
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-5.txt
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-6.txt
+keystepper --keyspace 1000000000:ffffffffff --keymask ennrrrrnnn --stridemap stridemap40.txt -c -b 46 -t 256 -p 256 1EeAxcprB2PpCnr34VfZdFrkUWuxyiNEFv -o found40-7.txt
+
