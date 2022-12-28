3deced80b611865b5961e29d7e5a6e0d54fef3e9
Dev McDevington <bill.dodd@graffitilogic.com>
2022-12-28 10:41:38 -0600

    drop samples that are no longer needed

--- Samples/test44.bat
+++
@@ -1,8 +0,0 @@
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-0.txt
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-1.txt
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-2.txt
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-3.txt
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-4.txt
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-5.txt
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-6.txt
-keyscanner --keyspace e0000000000:effffffffff --keymask errrrrrrnnn --stridemap stridemap44.txt -c -b 46 -t 256 -p 256 1CkR2uS7LmFwc3T2jV8C1BhWb5mQaoxedF -o found44-7.txt
--- Samples/test48.bat
+++
@@ -1,8 +0,0 @@
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-0.txt
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-1.txt
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-2.txt
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-3.txt
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-4.txt
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-5.txt
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-6.txt
-keyscanner --keyspace a00000000000:afffffffffff --keymask arrrrrrrrnnn --stridemap stridemap48.txt -c -b 46 -t 256 -p 256 1DFYhaB2J9q1LLZJWKTnscPWos9VBqDHzv -o found48-7.txt
